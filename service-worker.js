c226f0d466e24e0610038cbd9bf4efdef514f528
yes1949 <cptsytptpd@gmail.com>
2025-12-06 03:00:03 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vocab-app-cache-v2.8'; // 更新版本号！
+const CACHE_NAME = 'vocab-app-cache-v2.9'; // 更新版本号！
 const urlsToCache = [
   './',
   './index.html',
@@ -35,6 +35,7 @@ self.addEventListener('fetch', event => {
     caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
+
 
 
 
